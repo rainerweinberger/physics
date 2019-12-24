5f1bedb1cbab7954b7ccdd70836c957d63298960
rainer <rainer.weinberger@cfa.harvard.edu>
2019-12-23 22:44:00 -0500

    first unit tests

---
+++ tests/test_Physics.py
@@ -0,0 +1,25 @@
+#! /tests/test_Physics.py
+#
+# This file is part of physics.
+# Copyright (C) 2020  Rainer Weinberger (rainer.weinberger@cfa.harvard.edu)
+#
+# Physics is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Physics is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with physics.  If not, see <https://www.gnu.org/licenses/>.
+
+import pytest
+
+
+def test_PhysicsLoadable():
+    import physics
+    physics.print_copyright()
+    assert True
---
+++ tests/test_PhysicsConstants.py
@@ -0,0 +1,24 @@
+#! /tests/test_PhysicsConstants.py
+#
+# This file is part of physics.
+# Copyright (C) 2020  Rainer Weinberger (rainer.weinberger@cfa.harvard.edu)
+#
+# Physics is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Physics is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with physics.  If not, see <https://www.gnu.org/licenses/>.
+
+import pytest
+
+
+def test_PhysicsConstantsLoadable():
+    from physics import constants
+    assert constants.KILOPARSEC == pytest.approx(3.085678e+21)
