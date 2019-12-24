d6f979cf6b903e08bc60c1f186c4d0ae4c8347a6
rainer <rainer.weinberger@cfa.harvard.edu>
2019-12-23 22:12:00 -0500

    License information

---
+++ README.md
@@ -0,0 +1,7 @@
+# Physics
+
+Created by Rainer Weinberger, 2020 (rainer.weinberger@cfa.harvard.edu)
+
+This is a small python package that simplifies working with physics equations.
+So far mainly used for constants.
+
--- physics/constants/__init__.py
+++ physics/constants/__init__.py
@@ -1,5 +1,23 @@
+#! /physics/constants/__init__.py
+#
+# This file is part of physics.
+# Copyright (C) 2020  Rainer Weinberger (rainer.weinberger@cfa.harvard.edu)
+#
+# Physics is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Physics is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with physics.  If not, see <https://www.gnu.org/licenses/>.
+
 # often used physical constants (cgs units; NIST 2010)
-GRAVITY =  6.6738e-8
+GRAVITY = 6.6738e-8
 SOLAR_MASS = 1.989e33
 SOLAR_LUM = 3.826e33
 SOLAR_EFF_TEMP = 5.780e3
@@ -17,9 +35,9 @@ PROTONMASS = 1.67262178e-24
 ELECTRONMASS = 9.1093829e-28
 THOMPSON = 6.65245873e-25
 ELECTRONCHARGE = 4.8032042e-10
-HUBBLE = 3.2407789e-18         # in h/sec
-LYMAN_ALPHA = 1215.6e-8        # 1215.6 Angstroem
-LYMAN_ALPHA_HeII = 303.8e-8    # 303.8 Angstroem
+HUBBLE = 3.2407789e-18  # in h/sec
+LYMAN_ALPHA = 1215.6e-8  # 1215.6 Angstroem
+LYMAN_ALPHA_HeII = 303.8e-8  # 303.8 Angstroem
 OSCILLATOR_STRENGTH = 0.41615
 OSCILLATOR_STRENGTH_HeII = 0.41615
 ELECTRONVOLT_IN_ERGS = 1.60217656e-12
