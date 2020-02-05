78a0e9a07cc5d48f85117c278a51a0a0d7db475c
Rainer Weinberger <rainer.weinberger@cfa-harvard.edu>
2020-02-05 18:01:05 -0500

    small bugfix regarding coverage report in test.sh

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 rm -rf ./htmlcov
 # call python -m pytest instead of just pytest because this will add the pwd
 # to sys.path
-python -m pytest --cov-report html --cov=tests --cov-branch tests
+python -m pytest --cov-report html --cov=physics --cov-branch tests
