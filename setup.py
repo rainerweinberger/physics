34ab5f7ef72ee25c8ac66d79ef2ec58ce24e4297
Rainer Weinberger <rainer.weinberger@cfa-harvard.edu>
2020-06-19 20:42:26 -0400

    adjusting working directory for pytest

--- setup.py
+++ setup.py
@@ -3,6 +3,15 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
+def requirements(fname):
+    requires = []
+    with open(fname) as file:
+        for line in file:
+            requires.append(line.strip())
+    return list(requires)
+
+
 setuptools.setup(
     name="physics",
     version="0.0.1",
@@ -10,7 +19,7 @@ setuptools.setup(
     license="GNU General Public License v3 (GPLv3)",
     author_email="rainer.weinberger@cfa.harvard.edu",
     description="Useful physics stuff for python",
-    install_requires="numpy==1.18.2",
+    install_requires=requirements("requirements.txt"),
     long_description=long_description,
     long_description_content_type='text/markdown',
     url="https://github.com/rainerweinberger/physics",
